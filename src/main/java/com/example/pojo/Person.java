64aed426454545ccc21e1b3052ad921e74c85949
xuwz <xuwz@elanginfo.com>
2023-02-16 09:56:15 +0800

    initial project

---
+++ src/main/java/com/example/pojo/Person.java
@@ -0,0 +1,16 @@
+package com.example.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+    private String name;
+    private Integer salary;
+    private Integer age;
+    private String sex;
+    private String address;
+}
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.pojo.Person;
 import com.example.pojo.Student;
 import org.apache.maven.surefire.shade.org.apache.commons.lang3.StringUtils;
 import org.assertj.core.util.DateUtil;
@@ -8,21 +9,25 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
 
+import java.math.BigDecimal;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @SpringBootTest
 class DemoApplicationTests {
 
     @Test
     void contextLoads() {
-        Map< String,Integer> map = new HashMap<>();
-        map.put("橘子",6);
-        map.put("永泰",8);
-        map.put("恩佐",9);
+        Map<String, Integer> map = new HashMap<>();
+        map.put("橘子", 6);
+        map.put("永泰", 8);
+        map.put("恩佐", 9);
         Set<Map.Entry<String, Integer>> entries = map.entrySet();
         for (Map.Entry<String, Integer> entry : entries) {
             String key = entry.getKey();
@@ -38,9 +43,9 @@ class DemoApplicationTests {
         int b = 2;
         int c = 0;
         boolean flag = true;
-        if(!flag){
-          c = a + b;
-        }else{
+        if (!flag) {
+            c = a + b;
+        } else {
             c = a - b;
         }
         System.out.println(c);
@@ -64,7 +69,7 @@ class DemoApplicationTests {
     @Test
     void contextLoads5() {
         String name = "zhangsan";
-        Assert.isTrue(!"zhangsan".equals(name),"事项等的");
+        Assert.isTrue(!"zhangsan".equals(name), "事项等的");
     }
 
     @Test
@@ -81,9 +86,9 @@ class DemoApplicationTests {
 
     @Test
     void contextLoads7() {
-        List<String> list = Arrays.asList("1","2","3","4");
+        List<String> list = Arrays.asList("1", "2", "3", "4");
 //        Double collect = list.stream().collect(Collectors.averagingDouble(d -> d * 2));
-        String result=  list.stream().collect(Collectors.joining("*","(",")"));
+        String result = list.stream().collect(Collectors.joining("*", "(", ")"));
         System.out.println(result);
     }
 
@@ -96,8 +101,8 @@ class DemoApplicationTests {
 
     @Test
     void contextLoads9() {
-        List<String> args = Arrays.asList("ni","hao","world");
-        String collect = args.stream().collect(Collectors.joining(",","[","]"));
+        List<String> args = Arrays.asList("ni", "hao", "world");
+        String collect = args.stream().collect(Collectors.joining(",", "[", "]"));
         System.out.println(collect);
     }
 
@@ -119,7 +124,7 @@ class DemoApplicationTests {
     @Test
     void contextLoads12() {
         String name = "AfFBfnx";
-        String flag = name.substring(0,1).toLowerCase() + name.substring(1).toLowerCase();
+        String flag = name.substring(0, 1).toLowerCase() + name.substring(1).toLowerCase();
         String currentTime = LocalDateTime
                 .now()
                 .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
@@ -139,7 +144,7 @@ class DemoApplicationTests {
         strings.add("123");
         strings.add("abc");
         for (String string : strings) {
-            if("abc".equals(string)){
+            if ("abc".equals(string)) {
                 String s = string.toUpperCase();
                 System.out.println(s);
             }
@@ -152,14 +157,15 @@ class DemoApplicationTests {
         System.out.println("*****");
         List<String> arrayList = Lists.newArrayList(array);
         for (String s : arrayList) {
-            if("abc".equals(s)){
+            if ("abc".equals(s)) {
                 s.toUpperCase();
             }
             System.out.println(s);
         }
     }
+
     @Test
-    void contextLoads15(){
+    void contextLoads15() {
         // 创建一个 HashMap
         Map<Integer, String> sites = new HashMap<>();
         List<String> list = new ArrayList<>();
@@ -186,7 +192,7 @@ class DemoApplicationTests {
     }
 
     @Test
-    void contextLoads16(){
+    void contextLoads16() {
         List<Student> list = new ArrayList<>();
         list.add(new Student("张三", 19));
         list.add(new Student("王五", 19));
@@ -197,7 +203,7 @@ class DemoApplicationTests {
     }
 
     @Test
-    void contextLoads17(){
+    void contextLoads17() {
         Integer value1 = null;
         Integer value2 = new Integer(10);
 
@@ -206,10 +212,10 @@ class DemoApplicationTests {
 
         // Optional.of - 如果传递的参数是 null，抛出异常 NullPointerException
         Optional<Integer> b = Optional.of(value2);
-        System.out.println(sum(a,b));
+        System.out.println(sum(a, b));
     }
 
-    public Integer sum(Optional<Integer> a, Optional<Integer> b){
+    public Integer sum(Optional<Integer> a, Optional<Integer> b) {
 
         // Optional.isPresent - 判断值是否存在
 
@@ -222,6 +228,189 @@ class DemoApplicationTests {
         //Optional.get - 获取值，值需要存在
         Integer value2 = b.get();
         return value1 + value2;
+    }
+
+    @Test
+    void contextLoads18() {
+        List<Person> personList = new ArrayList<Person>();
+        personList.add(new Person("Tom", 8900, 23, "male", "New York"));
+        personList.add(new Person("Jack", 7000, 25, "male", "Washington"));
+        personList.add(new Person("Lily", 7800, 21, "female", "Washington"));
+        personList.add(new Person("Anni", 8200, 24, "female", "New York"));
+        personList.add(new Person("Owen", 9500, 25, "male", "New York"));
+        personList.add(new Person("Alisa", 7900, 26, "female", "New York"));
+
+        Map<String, Person> collect = personList.stream().filter(x -> x.getSalary() > 8000)
+                .collect(Collectors.toMap(x -> x.getName(), x -> x));
+        System.out.print("高于8000的员工姓名：" + collect);
+    }
+
+    @Test
+    void contextLoads19() {
+        List<String> list = Arrays.asList("adnm", "admmt", "pot", "xbangd", "weoujgsd");
+        Optional<String> max = list.stream().max(Comparator.comparing(x -> x.length()));
+//        System.out.println(max);
+
+        List<Integer> list2 = Arrays.asList(7, 6, 9, 4, 6);
+        Optional<Integer> max1 = list2.stream().max(Integer::compareTo);
+        Optional<Integer> max2 = list2.stream().max(Comparator.comparingInt(x -> x));
+//        System.out.println(max1);
+
+        List<Person> personList = new ArrayList<Person>();
+        personList.add(new Person("Tom", 8900, 23, "male", "New York"));
+        personList.add(new Person("Jack", 7000, 25, "male", "Washington"));
+        personList.add(new Person("Lily", 7800, 21, "female", "Washington"));
+        personList.add(new Person("Anni", 16200, 24, "female", "New York"));
+        personList.add(new Person("Owen", 7000, 25, "male", "New York"));
+        personList.add(new Person("Alisa", 8900, 26, "female", "New York"));
+
+        Optional<Integer> sumSalary = personList.stream().map(Person::getSalary).reduce(Integer::sum);
+        System.out.println(sumSalary);
+        List<Integer> collect = personList.stream().map(Person::getSalary).collect(Collectors.toList());
+        int sum = 0;
+        for (Integer i : collect) {
+            sum += i;
+
+        }
+//        System.out.println(sum);
+
+        Optional<Integer> reduce1 = personList.stream().map(Person::getSalary).reduce(Integer::sum);
+        Integer reduce2 = personList.stream().map(Person::getSalary).reduce(1, Integer::sum);
+//        System.out.println(reduce1);
+//        System.out.println(reduce2);
+        //按照工资升序
+        List<String> collect1 = personList.stream().sorted(Comparator.comparing(Person::getSalary)).map(Person::getName).collect(Collectors.toList());
+        System.out.println(collect1);
+        //按照工资升序,工资相同的话,按照名字长度降序
+        List<String> collect2 = personList.stream().sorted((s1, s2) -> {
+            if (Objects.equals(s1.getSalary(), s2.getSalary())) {
+                return s2.getName().length() - s1.getName().length();
+            } else {
+                return s1.getSalary() - s2.getSalary();
+            }
+        }).map(Person::getName).collect(Collectors.toList());
+        System.out.println(collect2);
+    }
+
+    @Test
+    void contextLoads20() {
+        Integer a = 10;
+        Long b = 11L;
+        long l = a.longValue();
+        int i = b.intValue();
+
+        System.out.println(l);
+        System.out.println();
+    }
+
+    @Test
+    void contextLoads21() {
+        System.out.println(String.format("%s,hhhh,%s", "6", "7"));
+    }
+
+    @Test
+    void contextLoads22() {
+        BigDecimal decimal = new BigDecimal("100");
+
+        System.out.println(decimal.multiply(new BigDecimal(1)));
+    }
+
+    @Test
+    void contextLoads23() {
+        String USERNAME_REGEX_EXP = "^[a-zA-Z]\\w{4,15}$";
+        String username = "p____0_";
+        Pattern pattern = Pattern.compile(USERNAME_REGEX_EXP);
+        Matcher matcher = pattern.matcher(username);
+        boolean flag = matcher.matches();
+        String ecos = flag == true ? username : "请输入正确的用户名!";
+        System.out.println(ecos);
+    }
+
+    @Test
+    void contextLoads25() {
+        String abc = camelToUnderline("DSgAcDDDccbC");
+        System.out.println(abc);
+    }
+    public static String camelToUnderline(String para){
+        int length = 3;
+        if(para.length()<length){
+            return para.toLowerCase();
+        }
+        StringBuilder sb=new StringBuilder(para);
+        //定位
+        int temp=0;
+        //从第三个字符开始 避免命名不规范
+        for(int i=2;i<para.length();i++){
+            if(Character.isUpperCase(para.charAt(i))){
+                sb.insert(i+temp, "_");
+                temp+=1;
+            }
+        }
+        return sb.toString().toLowerCase();
+    }
+
+    @Test
+    void contextLoads26() {
+        StringBuilder str = new StringBuilder("tutorials");
+        System.out.println("string = " + str);
+        CharSequence cSeq = "yiidian";
+// insert CharSequence at offset 9
+        str.insert(9, cSeq);
+// prints StringBuilder after insertion
+        System.out.println(str.toString());
+    }
+
+    @Test
+    void contextLoads27() {
+        System.out.println(camelName("_adas_DF_ssS_DFzDDz_cv"));
+    }
+
+    public static String camelName(String name) {
+        StringBuilder result = new StringBuilder();
+        // 快速检查
+        if (name == null || name.isEmpty()) {
+            // 没必要转换
+            return "";
+        } else if (!name.contains("_")) {
+            return name.substring(0, 1).toLowerCase() + name.substring(1).toLowerCase();
+            //update-end--Author:zhoujf  Date:20180503 for：TASK #2500 【代码生成器】代码生成器开发一通用模板生成功能
+        }
+        // 用下划线将原始字符串分割
+        String[] camels = name.split("_");
+        for (String camel : camels) {
+            // 跳过原始字符串中开头、结尾的下换线或双重下划线
+            if (camel.isEmpty()) {
+                continue;
+            }
+            // 处理真正的驼峰片段
+            if (result.length() == 0) {
+                // 第一个驼峰片段，全部字母都小写
+                result.append(camel.toLowerCase());
+            } else {
+                // 其他的驼峰片段，首字母大写
+                result.append(camel.substring(0, 1).toUpperCase());
+                result.append(camel.substring(1).toLowerCase());
+            }
+        }
+        return result.toString();
+    }
+
+    @Test
+    void contextLoads28() {
+        System.out.println(toCamelCase("asd_AdfgdB_BBf_f"));
+    }
+    public static String toCamelCase(String inputString) {
+        String[] parts = inputString.split("_");
+        StringBuilder camelCaseString = new StringBuilder();
+        for (String part : parts) {
+            if (camelCaseString.length() == 0) {
+                camelCaseString.append(part);
+            } else {
+                camelCaseString.append(part.substring(0, 1).toUpperCase());
+                camelCaseString.append(part.substring(1));
+            }
+        }
+        return camelCaseString.toString();
     }
 
 }
